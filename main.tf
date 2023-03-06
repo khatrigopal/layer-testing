dca4181a52a70f5276ca43d2abd6fbd66facfb51
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-06 17:03:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 module "my_lambda_layer" {
   source = "./modules/layer"
 
-  layer_name      = "my-layer"
+  layer_name      = "my-layer1"
   s3_bucket_name  = "bskhatrig-testing"
-  s3_key_prefix   = "layers/my-layer"
+  s3_key_prefix   = "layers/my-layer1"
 
 }
 
+  layer_name      = "my-layer2"
+  s3_bucket_name  = "bskhatrig-testing"
+  s3_key_prefix   = "layers/my-layer2"
+
+}
 output "my_lambda_layer_arn" {
   value = module.my_lambda_layer.layer_arn
 }
